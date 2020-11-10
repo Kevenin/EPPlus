8b17b005c4b322e6af745e606937e0af57eb6ba9
Kevenin <kblanchet@live.com>
2020-11-10 08:11:25 -0500

    Update ZipOutputStream.cs
    Commented line to fix zip header for email attachment

--- EPPlus/Packaging/DotNetZip/ZipOutputStream.cs
+++ EPPlus/Packaging/DotNetZip/ZipOutputStream.cs
@@ -1386,7 +1386,7 @@ namespace OfficeOpenXml.Packaging.Ionic.Zip
             _FinishCurrentEntry();
             _currentEntry = ZipEntry.CreateForZipOutputStream(entryName);
             _currentEntry._container = new ZipContainer(this);
-            _currentEntry._BitField |= 0x0008;  // workitem 8932
+            //_currentEntry._BitField |= 0x0008;  // workitem 8932
             _currentEntry.SetEntryTimes(DateTime.Now, DateTime.Now, DateTime.Now);
             _currentEntry.CompressionLevel = this.CompressionLevel;
             _currentEntry.CompressionMethod = this.CompressionMethod;
